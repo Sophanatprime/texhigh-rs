3d696ace052f8d810721b98211fc3139aa1bbd5c
Sophanatprime <2757172594@qq.com>
2025-08-18 10:34:12 +0800

    release: v0.3.2

--- src/lib.rs
+++ src/lib.rs
@@ -73,7 +73,7 @@ use crate::{
 };
 
 const FULL_VERSION: &str = env!("CARGO_PKG_VERSION");
-const DATE: &str = "2025/07/30";
+const DATE: &str = "2025/08/18";
 const COPYRIGHT: &str = "2024-2025, Wenjian Chern";
 
 pub fn print_copyright(s: &'static str) {
