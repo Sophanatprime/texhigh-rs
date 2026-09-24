a99e3220cc32bdbbce6dc4f79809dba957861cba
Sophanatprime <2757172594@qq.com>
2026-09-24 22:01:31 +0800

    release: 0.4.1

--- src/lib.rs
+++ src/lib.rs
@@ -71,8 +71,8 @@ use crate::{
 };
 
 const FULL_VERSION: &str = env!("CARGO_PKG_VERSION");
-const DATE: &str = "2025/09/24";
-const COPYRIGHT: &str = "2024-2025, Wenjian Chern";
+const DATE: &str = "2026/09/24";
+const COPYRIGHT: &str = "2024-2026, Wenjian Chern";
 
 pub fn print_copyright(s: &'static str) {
     println!(
